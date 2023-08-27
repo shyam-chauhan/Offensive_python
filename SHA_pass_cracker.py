261c28d60e543d985ef9dd50b1d59a641cc91823
SHYAM <mitsme44@gmail.com>
2023-08-27 14:43:53 +0530

    initial commit

---
+++ SHA_pass_cracker.py
@@ -0,0 +1,28 @@
+import hashlib
+
+def read_dictionary(file_path):
+	with open(file_path, 'r') as file:
+		return [line.strip() for line in file]
+
+def hash_password(password):
+	return hashlib.sha256(password.encode()).hexdigest()
+
+
+def crack_password(target_hash,dictionary):
+	for word in dictionary:
+		hashed_word = hash_password(word)
+		if hashed_word == target_hash:
+			return word
+	return None
+
+if __name__ == "__main__":
+
+	target_hash = "2d00c503a7a9910fa619bd03fe770cd3b47606d18c327a8dc191cee2770dd7ab"
+	dictionary_file = "dictionary.txt"
+	dictionary = read_dictionary(dictionary_file)
+	cracked_password = crack_password(target_hash, dictionary)
+
+	if cracked_password:
+		print(f"Password cracked, the password is : {cracked_password}")
+	else:
+		print("Password not found in dictionaly")
