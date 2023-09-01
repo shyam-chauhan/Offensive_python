c227ad2e24a512111b9ef6306a1aad0a37478ccf
SHYAM <mitsme44@gmail.com>
2023-09-01 17:41:25 +0530

    Zip cracker updated and README.md added

---
+++ zip_cracker/README.md
@@ -0,0 +1,3 @@
+#zip cracker
+
+Default password of crackit.zip is SYM
--- zip_cracker/zip_brute.py
+++ zip_cracker/zip_brute.py
@@ -13,7 +13,7 @@ class zip_crack:
     def info_in():
         global zip_file,char_set,max_pass_len
         zip_file = input("Path to your zipfile :")
-        char_set = input("Provide character set that you want to use to crack password, DON'T REPEAT (default is a-z,A-Z,0-9) : ") or "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+        char_set = input("Provide character set that you want to use to crack password, DON'T REPEAT (default is a-z,A-Z,0-9) : ") or "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
         max_pass_len = int(input("Provide maximum possible length of password(It will take time accordingly!) : "))
 
 
@@ -21,7 +21,7 @@ class zip_crack:
         global zip_file,char_set,max_pass_len
         zip_file = zipfile.ZipFile(zip_file)
 
-        #print("Total password to test : ", total_tries())
+        print("Total password to test : ", len(char_set) ** max_pass_len)
     
         for password_length in range(1, max_pass_len + 1):
             for word in tqdm(itertools.product(char_set, repeat=max_pass_len), total=len(char_set) ** max_pass_len, unit="word",ascii =' //'):
@@ -36,10 +36,6 @@ class zip_crack:
 
         print("[!] Password not found, try longer password or diffrent characterset.")
 
-
-
-    def total_tries():
-        return (len(char_set) ** max_pass_len)
 
 
 if __name__ == "__main__":
