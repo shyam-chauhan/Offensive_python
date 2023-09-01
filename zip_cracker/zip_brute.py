d7fd1c4b7eab80c2bbd644acf2e073d28cea7d65
SHYAM <mitsme44@gmail.com>
2023-09-01 17:16:42 +0530

    Zip cracker updated

--- zip_cracker/zip_brute.py
+++ zip_cracker/zip_brute.py
@@ -2,26 +2,47 @@ import zipfile
 from tqdm import tqdm
 import itertools
 
-zip_file = "crackit.zip" #Path to your zipfile goes here
-zip_file = zipfile.ZipFile(zip_file)
 
-character_set = "abcdefghijklmopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+class zip_crack:
 
-max_pass_length = 4
+    def __init__():
+        zip_file = ""
+        char_set = ""
+        max_pass_len = 0
 
-n_words = len(character_set) ** max_pass_length
-print("Total password to test : ", n_words)
+    def info_in():
+        global zip_file,char_set,max_pass_len
+        zip_file = input("Path to your zipfile :")
+        char_set = input("Provide character set that you want to use to crack password, DON'T REPEAT (default is a-z,A-Z,0-9) : ") or "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+        max_pass_len = int(input("Provide maximum possible length of password(It will take time accordingly!) : "))
 
 
-for password_length in range(1, max_pass_length + 1):
-    for word in tqdm(itertools.product(character_set, repeat=password_length), total=len(character_set) ** password_length, unit="word",ascii =' //'):
-        password = "".join(word)
-        try:
-            zip_file.extractall(pwd=password.encode())
-        except:
-            continue
-        else:
-            print("\n[+] Password found : ", password)
-            exit(0)
+    def info_processor():
+        global zip_file,char_set,max_pass_len
+        zip_file = zipfile.ZipFile(zip_file)
 
-print("[!] Password not found, try longer password or diffrent characterset.")
+        #print("Total password to test : ", total_tries())
+    
+        for password_length in range(1, max_pass_len + 1):
+            for word in tqdm(itertools.product(char_set, repeat=max_pass_len), total=len(char_set) ** max_pass_len, unit="word",ascii =' //'):
+                password = "".join(word)
+                try:
+                    zip_file.extractall(pwd=password.encode())
+                except:
+                    continue
+                else:
+                    print("\n[+] Password found : ", password)
+                    exit(0)
+
+        print("[!] Password not found, try longer password or diffrent characterset.")
+
+
+
+    def total_tries():
+        return (len(char_set) ** max_pass_len)
+
+
+if __name__ == "__main__":
+    z = zip_crack
+    z.info_in()
+    z.info_processor()
