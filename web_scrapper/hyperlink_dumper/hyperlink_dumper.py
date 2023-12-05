59bfe3f500d849a9f0ceca06909395b11833be0f
shyam-chauhan <mitsme44@gmail.com>
2023-12-05 21:25:54 +0530

    code updated

--- web_scrapper/hyperlink_dumper/hyperlink_dumper.py
+++ web_scrapper/hyperlink_dumper/hyperlink_dumper.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://ldce.ac.in' #url of webpages from where you want to scrap hyperlinks
+url = 'https://' #url of webpages from where you want to scrap hyperlinks
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}  #Request headers masqurading with windows firefox
 
